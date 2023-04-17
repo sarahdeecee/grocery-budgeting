ced21802dd9326f95e35e80012474ba63a04c946
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-04-17 18:26:37 -0400

    fix new item widths

--- src/components/NewItem.tsx
+++ src/components/NewItem.tsx
@@ -1,5 +1,5 @@
 import { AddAPhotoRounded, AddCircle, Delete } from "@mui/icons-material";
-import { Box, Divider, FormControl, FormHelperText, Grid, IconButton, InputBase, List, ListItem, OutlinedInput, useFormControl } from "@mui/material";
+import { Box, Divider, FormControl, FormHelperText, Grid, IconButton, InputAdornment, List, ListItem, OutlinedInput, useFormControl, Input } from "@mui/material";
 import { useMemo, useState } from "react";
 
 function MyFormHelperText() {
@@ -37,11 +37,15 @@ function NewItem() {
       }
       sx={{width: '100%'}}
       >
-      <Box component="form" noValidate autoComplete="off">
+      <Box component="form" noValidate autoComplete="off" sx={{width: '100%'}}>
         <FormControl sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%'}}>
-          <InputBase placeholder="Item name" sx={{ ml: 1, flex: 1, width: '50ch' }} />
+          <Input placeholder="Item name" sx={{ml: 1, width: '75%'}} />
           <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          <InputBase placeholder="Price" sx={{ ml: 1}} />
+          <Input
+            placeholder="Price"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            sx={{ml: 1, width: '20%', flexGrow: 1}}
+          />
         {/* <MyFormHelperText /> */}
         </FormControl>
       </Box>
