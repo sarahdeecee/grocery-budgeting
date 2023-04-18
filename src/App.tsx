d79e5f8820dd903fe4d6f8fb2c8dd5da143e981e
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-04-18 13:13:24 -0400

    add app bar to footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ItemList from './components/ItemList';
 import NewItem from './components/NewItem';
 import { ItemType } from './Types';
 import { useState } from 'react';
+import Footer from './components/Footer';
 
 const itemsDefault: ItemType[] = [
   {
@@ -36,6 +37,72 @@ const itemsDefault: ItemType[] = [
     priceCents: 500,
     hasTax: true
   },
+  {
+    name: 'Bananas',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Pop',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Muffins',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Sugar',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Coffee',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Creamer',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Salt',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Juice',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Cake',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Asparagus',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
+  {
+    name: 'Spinach',
+    quantity: 1,
+    priceCents: 100,
+    hasTax: true
+  },
 ];
 
 const blankItem = {
@@ -49,11 +116,13 @@ function App() {
   const [items, setItems] = useState<ItemType[]>(itemsDefault);
   const [currentItem, setCurrentItem] = useState<ItemType>(blankItem);
 
-  return (
-    <Stack className="App">
-      <NewItem items={items} setItems={setItems} />
-      <ItemList items={items} setItems={setItems} currentItem={currentItem} setCurrentItem={setCurrentItem} />
-    </Stack>
+  return (<>
+      <Stack className="App">
+        {/* <NewItem items={items} setItems={setItems} /> */}
+        <ItemList items={items} setItems={setItems} currentItem={currentItem} setCurrentItem={setCurrentItem} />
+      </Stack>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,40 @@
+import styled from "@emotion/styled";
+import { Add, Menu, More, Search } from "@mui/icons-material";
+import { AppBar, Box, Fab, IconButton, Toolbar, Typography } from "@mui/material";
+
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+});
+
 function Footer() {
 
   return (
-    <></>
+    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer">
+            <Menu />
+          </IconButton>
+          <Typography
+            noWrap
+            sx={{left: 0, right: 0}}
+          >Total: $0.00</Typography>
+          <StyledFab color="success" aria-label="add">
+            <Add />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <Search />
+          </IconButton>
+          <IconButton color="inherit">
+            <More />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
   );
 }
 
