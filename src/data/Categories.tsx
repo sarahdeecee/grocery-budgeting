30e6a232cbcbcbb13d3bde858a559206d31ffa56
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-05-03 00:50:33 -0400

    add common products with categories

---
+++ src/data/Categories.tsx
@@ -0,0 +1,550 @@
+export const commonItems = [
+  {
+    name: 'Broccoli',
+    category: 'Produce'
+  },
+  {
+    name: 'Cauliflower',
+    category: 'Produce'
+  },
+  {
+    name: 'Carrots',
+    category: 'Produce'
+  },
+  {
+    name: 'Celery',
+    category: 'Produce'
+  },
+  {
+    name: 'Cucumbers',
+    category: 'Produce'
+  },
+  {
+    name: 'Mushrooms',
+    category: 'Produce'
+  },
+  {
+    name: 'Lettuce',
+    category: 'Produce'
+  },
+  {
+    name: 'Tomatoes',
+    category: 'Produce'
+  },
+  {
+    name: 'Onions',
+    category: 'Produce'
+  },
+  {
+    name: 'Peppers',
+    category: 'Produce'
+  },
+  {
+    name: 'Green Beans',
+    category: 'Produce'
+  },
+  {
+    name: 'salad mix',
+    category: 'Produce'
+  },
+  {
+    name: 'Potatoes',
+    category: 'Produce'
+  },
+  {
+    name: 'Garlic',
+    category: 'Produce'
+  },
+  {
+    name: 'Apples',
+    category: 'Produce'
+  },
+  {
+    name: 'Oranges',
+    category: 'Produce'
+  },
+  {
+    name: 'Bananas',
+    category: 'Produce'
+  },
+  {
+    name: 'Strawberries',
+    category: 'Produce'
+  },
+  {
+    name: 'Grapes',
+    category: 'Produce'
+  },
+  {
+    name: 'Raspberries',
+    category: 'Produce'
+  },
+  {
+    name: 'Blackberries',
+    category: 'Produce'
+  },
+  {
+    name: 'Lemons',
+    category: 'Produce'
+  },
+  {
+    name: 'Limes',
+    category: 'Produce'
+  },
+  {
+    name: 'Cantaloupe',
+    category: 'Produce'
+  },
+  {
+    name: 'Peaches',
+    category: 'Produce'
+  },
+  {
+    name: 'Pineapple',
+    category: 'Produce'
+  },
+  {
+    name: 'Avocado',
+    category: 'Produce'
+  },
+  {
+    name: 'Ground Beef',
+    category: 'Meat'
+  },
+  {
+    name: 'Ground Chicken',
+    category: 'Meat'
+  },
+  {
+    name: 'Ground Turkey',
+    category: 'Meat'
+  },
+  {
+    name: 'Ground Pork',
+    category: 'Meat'
+  },
+  {
+    name: 'Sausage',
+    category: 'Meat'
+  },
+  {
+    name: 'Bacon',
+    category: 'Meat'
+  },
+  {
+    name: 'Chicken Breast',
+    category: 'Meat'
+  },
+  {
+    name: 'Chicken Thigh',
+    category: 'Meat'
+  },
+  {
+    name: 'Chicken Quarters',
+    category: 'Meat'
+  },
+  {
+    name: 'Whole Chicken',
+    category: 'Meat'
+  },
+  {
+    name: 'Pork',
+    category: 'Meat'
+  },
+  {
+    name: 'Beef',
+    category: 'Meat'
+  },
+  {
+    name: 'Burgers',
+    category: 'Meat'
+  },
+  {
+    name: 'Hot Dogs',
+    category: 'Meat'
+  },
+  {
+    name: 'Frozen Fish',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Fish',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Crab',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Shrimp',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Sushi',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Tuna',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Canned Tuna',
+    category: 'Fish & Seafood'
+  },
+  {
+    name: 'Chips',
+    category: 'Snacks'
+  },
+  {
+    name: 'Chocolate Bars',
+    category: 'Snacks'
+  },
+  {
+    name: 'Cookies',
+    category: 'Snacks'
+  },
+  {
+    name: 'Candy',
+    category: 'Snacks'
+  },
+  {
+    name: 'Nuts',
+    category: 'Snacks'
+  },
+  {
+    name: 'Pretzels',
+    category: 'Snacks'
+  },
+  {
+    name: 'Popcorn',
+    category: 'Snacks'
+  },
+  {
+    name: 'Water',
+    category: 'Beverages'
+  },
+  {
+    name: 'Juice',
+    category: 'Beverages'
+  },
+  {
+    name: 'Pop',
+    category: 'Beverages'
+  },
+  {
+    name: 'Coffee',
+    category: 'Beverages'
+  },
+  {
+    name: 'Tea',
+    category: 'Beverages'
+  },
+  {
+    name: 'Beer',
+    category: 'Beverages'
+  },
+  {
+    name: 'Wine',
+    category: 'Beverages'
+  },
+  {
+    name: 'Milk',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'Cream',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'Coffee Creamer',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'almond milk',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'soy milk',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'cheese',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'butter',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'margarine',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'yogurt',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'sour cream',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'ice cream',
+    category: 'Dairy & Alternatives'
+  },
+  {
+    name: 'bread',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'bagels',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'muffins',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'buns',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'rice',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'pasta',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'cereal',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'tortillas',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'hamburger buns',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'hotdog buns',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'pie',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'cookies',
+    category: 'Grains & Bakery'
+  },
+  {
+    name: 'paper towels',
+    category: 'Household'
+  },
+  {
+    name: 'dish soap',
+    category: 'Household'
+  },
+  {
+    name: 'laundry detergent',
+    category: 'Household'
+  },
+  {
+    name: 'toilet paper',
+    category: 'Household'
+  },
+  {
+    name: 'tissues',
+    category: 'Household'
+  },
+  {
+    name: 'aluminum foil',
+    category: 'Household'
+  },
+  {
+    name: 'sponges',
+    category: 'Household'
+  },
+  {
+    name: 'plastic bags',
+    category: 'Household'
+  },
+  {
+    name: 'trash bags',
+    category: 'Household'
+  },
+  {
+    name: 'soap',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'shampoo',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'conditioner',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'deodorant',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'body wash',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'face wash',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'moisturizer',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'lotion',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'toothpaste',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'mouthwash',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'medicine',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'shaving cream',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'vitamins',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'protein powder',
+    category: 'Health & Beauty'
+  },
+  {
+    name: 'cat food',
+    category: 'Pet Supplies'
+  },
+  {
+    name: 'dog food',
+    category: 'Pet Supplies'
+  },
+  {
+    name: 'cat litter',
+    category: 'Pet Supplies'
+  },
+  {
+    name: 'cat treats',
+    category: 'Pet Supplies'
+  },
+  {
+    name: 'dog treats',
+    category: 'Pet Supplies'
+  },
+  {
+    name: 'salt',
+    category: 'Pantry'
+  },
+  {
+    name: 'sugar',
+    category: 'Pantry'
+  },
+  {
+    name: 'flour',
+    category: 'Pantry'
+  },
+  {
+    name: 'cake mix',
+    category: 'Pantry'
+  },
+  {
+    name: 'frosting',
+    category: 'Pantry'
+  },
+  {
+    name: 'hot sauce',
+    category: 'Pantry'
+  },
+  {
+    name: 'ketchup',
+    category: 'Pantry'
+  },
+  {
+    name: 'mustard',
+    category: 'Pantry'
+  },
+  {
+    name: 'mayonnaise',
+    category: 'Pantry'
+  },
+  {
+    name: 'pickles',
+    category: 'Pantry'
+  },
+  {
+    name: 'barbecue sauce',
+    category: 'Pantry'
+  },
+  {
+    name: 'peanut butter',
+    category: 'Pantry'
+  },
+  {
+    name: 'soy sauce',
+    category: 'Pantry'
+  },
+  {
+    name: 'salad dressing',
+    category: 'Pantry'
+  },
+  {
+    name: 'oil',
+    category: 'Pantry'
+  },
+  {
+    name: 'olive oil',
+    category: 'Pantry'
+  },
+  {
+    name: 'vegetable oil',
+    category: 'Pantry'
+  },
+  {
+    name: 'honey',
+    category: 'Pantry'
+  },
+  {
+    name: 'vanilla',
+    category: 'Pantry'
+  },
+  {
+    name: 'jam',
+    category: 'Pantry'
+  },
+  {
+    name: 'jelly',
+    category: 'Pantry'
+  },
+  {
+    name: 'maple syrup',
+    category: 'Pantry'
+  },
+  {
+    name: 'salsa',
+    category: 'Pantry'
+  },
+  {
+    name: '',
+    category: ''
+  },
+];
