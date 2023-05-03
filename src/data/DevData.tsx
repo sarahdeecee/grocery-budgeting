bb8638ba91a6f8affb0edc3030c1f8e3ce2fa227
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-05-03 01:44:01 -0400

    update dev data for testing

--- src/data/DevData.tsx
+++ src/data/DevData.tsx
@@ -7,111 +7,135 @@ export const itemsDefault: ItemType[] = [
     priceCents: 100,
     tax: 0,
     notes: "It's super tasty",
-    checked: false
+    checked: false,
+    category: 'Produce'
   },
   {
     name: 'Orange',
     quantity: 1,
     priceCents: 500,
     tax: 0,
-    checked: false
+    checked: false,
+    category: 'Produce'
   },
   {
     name: 'Milk',
     quantity: 1,
     priceCents: 500,
     tax: 0,
-    checked: false
+    checked: false,
+    category: 'Dairy & Alternatives'
   },
   {
     name: 'Bread',
     quantity: 1,
     priceCents: 500,
     tax: 5,
-    checked: false
+    checked: false,
+    category: 'Grains & Bakery'
   },
   {
     name: 'Cookies',
     quantity: 1,
     priceCents: 500,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Grains & Bakery'
   },
   {
     name: 'Bananas',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Produce'
   },
   {
     name: 'Pop',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Beverages'
   },
   {
     name: 'Muffins',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Grains & Bakery'
   },
   {
     name: 'Sugar',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Pantry'
   },
   {
     name: 'Coffee',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Beverages'
   },
   {
     name: 'Creamer',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Dairy & Alternatives'
   },
   {
     name: 'Salt',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Pantry'
   },
   {
     name: 'Juice',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Beverages'
   },
   {
     name: 'Cake',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Grains & Bakery'
   },
   {
     name: 'Asparagus',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Produce'
   },
   {
     name: 'Spinach',
     quantity: 1,
     priceCents: 100,
     tax: 13,
-    checked: false
+    checked: false,
+    category: 'Produce'
+  },
+  {
+    name: 'Chips',
+    quantity: 1,
+    priceCents: 199,
+    tax: 13,
+    checked: false,
+    category: 'Snacks'
   },
 ];
