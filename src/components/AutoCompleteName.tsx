380e86bd480f763ef71224c35f58f154afe51e7e
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-06-07 11:38:39 -0400

    fix add item when focus lost

--- src/components/AutoCompleteName.tsx
+++ src/components/AutoCompleteName.tsx
@@ -2,6 +2,7 @@ import TextField from '@mui/material/TextField';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { commonItems } from '../data/Categories';
 import { CommonItem } from '../Types';
+import { camelCaseTrim } from '../helpers/Helpers';
 
 const filter = createFilterOptions<CommonItem>();
 
@@ -11,16 +12,19 @@ export default function AutoCompleteName(props: any) {
   return (
     <Autocomplete
       value={newItem.name}
-      onChange={(e: React.SyntheticEvent, newValue) => {
-        if (newValue && newValue.inputValue) {
+      onInputChange={(e: React.SyntheticEvent, newValue) => {
+        const isExisting = commonItems.some((option) => newValue.toLowerCase() === option.name.toLowerCase());
+        if (newValue && !isExisting) {
           // Create a new value from the user input
-          setNewItem({...newItem, name: newValue.inputValue, category: 'Other'});
+          setNewItem({...newItem, name: newValue, category: 'Other'});
         } else if (newValue === null) {
           // Clear form if null
           setNewItem({...newItem, name: '', category: 'Other'});
-        } else if (typeof newValue.category === 'string') {
+        } else if (isExisting) {
           // Add category if common item
-          setNewItem({...newItem, name: newValue.name, category: newValue.category});
+          const foundItem = commonItems.find((item: CommonItem) => item.name === newValue)
+          const foundItemCategory = foundItem ? foundItem.category : 'Other';
+          setNewItem({...newItem, name: newValue, category: foundItemCategory});
         }
       }}
       filterOptions={(options, params) => {
@@ -28,22 +32,21 @@ export default function AutoCompleteName(props: any) {
 
         const { inputValue } = params;
         // Suggest the creation of a new value
-        const isExisting = options.some((option) => inputValue === option.name);
+        const isExisting = options.some((option) => inputValue.toLowerCase() === option.name.toLowerCase());
         if (inputValue !== '' && !isExisting) {
           filtered.push({
             inputValue,
-            name: `Add "${inputValue}"`,
+            name: `${camelCaseTrim(inputValue)}`,
           });
         }
 
         return filtered;
       }}
-      selectOnFocus
-      clearOnBlur
+      // selectOnFocus
       handleHomeEndKeys
       id="item-name-box"
       options={commonItems}
-      groupBy={(option) => option.category ? option.category : 'Other'}
+      groupBy={(option) => option.category ? option.category : ''}
       getOptionLabel={(option) => {
         // Value selected with enter, right from the input
         if (typeof option === 'string') {
@@ -59,7 +62,14 @@ export default function AutoCompleteName(props: any) {
       renderOption={(props, option) => <li {...props}>{option.name}</li>}
       freeSolo
       renderInput={(params) => (
-        <TextField {...params} label="Item" variant="standard" autoFocus />
+        <TextField {...params}
+          label="Item"
+          variant="standard"
+          InputProps={{
+            ...params.InputProps,
+            // type: 'search',
+          }}
+        />
       )}
     />
   );
